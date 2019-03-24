68d854716a248c88103934562c10a44d2204909c
Brian Tipold <btipold@gmail.com>
2019-03-24 12:44:16 -0400

    Update NumDisplay.h

--- NumDisplay.h
+++ NumDisplay.h
@@ -4,9 +4,21 @@
 
 // LIBRARIES
 #include "eBike.h"
+            
+// LOOKUP TABLES
+bool init_sequence[10][8] = {
+            { LOW, HIGH, LOW, LOW, LOW, LOW, LOW, LOW },    
+            { LOW, LOW, HIGH, LOW, LOW, LOW, LOW, LOW },    
+            { LOW, LOW, LOW, HIGH, LOW, LOW, LOW, LOW },    
+            { HIGH, LOW, LOW, LOW, LOW, LOW, LOW, LOW },    
+            { LOW, LOW, LOW, LOW, LOW, LOW, LOW, HIGH },    
+            { LOW, LOW, LOW, LOW, LOW, LOW, HIGH, LOW },
+            { LOW, LOW, LOW, LOW, LOW, HIGH, LOW, LOW },
+            { HIGH, LOW, LOW, LOW, LOW, LOW, LOW, LOW }   };
+			
 
-// LOOKUP TABLE
-bool digits[10][8] = {  { LOW,  HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH  },    // 0
+bool digits[10][8] = {  
+            { LOW,  HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH },    // 0
             { LOW,  LOW,  LOW,  HIGH, LOW, HIGH, LOW, LOW   },    // 1
             { HIGH, LOW,  HIGH, HIGH, LOW, LOW,  HIGH, HIGH },    // 2
             { HIGH, LOW,  HIGH, HIGH, LOW, HIGH, HIGH, LOW  },    // 3
@@ -16,21 +28,41 @@ bool digits[10][8] = {  { LOW,  HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH  },    /
             { LOW,  LOW,  HIGH, HIGH, LOW, HIGH, LOW,  LOW  },    // 7
             { HIGH, HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH },    // 8
             { HIGH, HIGH, HIGH, HIGH, LOW, HIGH, LOW,  LOW  }  }; // 9
-            
+
 // DISPLAY NUMBER
-void DisplayNumber(int num) {
-	int left_digit = num / 10;
-	int right_digit = num % 10;
-	digitalWrite(LATCH, LOW);
-	for (int i(0); i < 8; ++i) {
-		digitalWrite(CLOCK, LOW);
-		digitalWrite(SHIFT_IN, digits[right_digit][i]);
-		digitalWrite(CLOCK, HIGH);
-	}
-	for (int i(0); i < 8; ++i) {
-		digitalWrite(CLOCK, LOW);
-		digitalWrite(SHIFT_IN, digits[left_digit][i]);
-		digitalWrite(CLOCK, HIGH);
-	}
-	digitalWrite(LATCH, HIGH);
+void DisplayNumber(const int num) {
+  int left_digit = num / 10;
+  int right_digit = num % 10;
+  digitalWrite(LATCH, LOW);
+  for (int i(0); i < 8; ++i) {
+    digitalWrite(CLOCK, LOW);
+    digitalWrite(SHIFT_IN, digits[right_digit][i]);
+    digitalWrite(CLOCK, HIGH);
+  }
+  for (int i(0); i < 8; ++i) {
+    digitalWrite(CLOCK, LOW);
+    digitalWrite(SHIFT_IN, digits[left_digit][i]);
+    digitalWrite(CLOCK, HIGH);
+  }
+  digitalWrite(LATCH, HIGH);
+}
+
+void start_up(void) {
+  for (int k(0); k < 4; k++) {
+    for (int i(0); i < 8; ++i) {
+      digitalWrite(LATCH, LOW);
+      for (int j(0); j < 8; ++j) {
+        digitalWrite(CLOCK, LOW);
+        digitalWrite(SHIFT_IN, init_sequence[i][j]);
+        digitalWrite(CLOCK, HIGH);
+      }
+      for (int j(0); j < 8; ++j) {
+        digitalWrite(CLOCK, LOW);
+        digitalWrite(SHIFT_IN, init_sequence[i][j]);
+        digitalWrite(CLOCK, HIGH);
+      }
+      delay(100);
+      digitalWrite(LATCH, HIGH);
+    }
+  }
 }
